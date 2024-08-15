a62e04655bf601692c8073e6352df3c7063dfe9f
burak bilici <burakbilici2014@gmail.com>
2024-08-15 13:34:29 +0300

    added .env

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,12 +1,14 @@
-import { NextRequest, NextResponse } from 'next/server';
-import axios from 'axios';
-import { cookies } from 'next/headers';
+import { NextRequest, NextResponse } from "next/server";
+import axios from "axios";
+import { cookies } from "next/headers";
 
 export async function POST(request: NextRequest) {
+  const baseUrl: string | undefined = process.env.NEXT_PUBLIC_WEBSITE_URL;
+
   const { userName, password } = await request.json();
 
   try {
-    const response = await axios.post('https://service.rmosweb.com/security/createToken', {
+    const response = await axios.post(baseUrl ?? "", {
       userName,
       password,
     });
@@ -15,21 +17,24 @@ export async function POST(request: NextRequest) {
       const token = response.data;
 
       const cookieStore = cookies();
-      cookieStore.set('token', token, {
+      cookieStore.set("token", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
-        sameSite: 'strict',
-        maxAge:  60,  
-        path: '/',
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "strict",
+        maxAge: 60,
+        path: "/",
       });
 
       return NextResponse.json({ success: true });
     }
   } catch (error: any) {
-    const errorMessage = axios.isAxiosError(error) && error.response
-      ? error.response.data?.message || 'An error occurred'
-      : 'An error occurred';
+    const errorMessage =
+      axios.isAxiosError(error) && error.response
+        ? error.response.data?.message || "An error occurred"
+        : "An error occurred";
 
-    return new NextResponse(errorMessage, { status: error.response?.status || 500 });
+    return new NextResponse(errorMessage, {
+      status: error.response?.status || 500,
+    });
   }
 }
